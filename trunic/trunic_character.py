15e40894602c034b8894eb965a9436c33ba1c0d5
Ian <ianwohlers@gmail.com>
2023-01-22 20:04:17 -0800

    Create tuple storage

--- trunic/trunic_character.py
+++ trunic/trunic_character.py
@@ -1,6 +1,6 @@
 from constant.edges import Edge, Edges
 from constant.sounds import IPA_TO_TRUNIC_EDGES
-from typing import FrozenSet
+from typing import FrozenSet, NamedTuple
 
 Edges = FrozenSet[Edge]
 
@@ -10,3 +10,8 @@ class TrunicCharacter:
 
     def get_edges(self) -> Edges:
         return self.__edges
+
+# Alternative idea on how to store Trunic characters (hashable):
+class TrunicTuple(NamedTuple):
+    edges: Edges
+    dot: bool
